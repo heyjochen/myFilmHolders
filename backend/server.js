f97dd6bc5a314afc5c7897f78f7b66521dd2a67a
heyjochen <jochen@stierberger.com>
2022-07-04 14:19:19 -0500

    setup router

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,9 @@
-console.log("hell");
+const express = require("express");
+const dotenv = require("dotenv").config();
+const port = process.env.PORT || 8000;
+
+const app = express();
+
+app.use("/api/projects", require("./routes/projectRoutes"));
+
+app.listen(port, () => console.log(`We runnin on port ${port}`));
