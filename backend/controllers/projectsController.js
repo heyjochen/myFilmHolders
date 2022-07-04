a6519a65344859145f4429cf73445f7e994a67a1
heyjochen <jochen@stierberger.com>
2022-07-04 15:01:16 -0500

    import asyncHandler

--- backend/controllers/projectsController.js
+++ backend/controllers/projectsController.js
@@ -1,27 +1,33 @@
+const asyncHandler = require("express-async-handler");
+
 // @desc Get projects
 // @route GET /api/projects
 // @access Private
-const getProjects = (req, res) => {
+const getProjects = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "Get projects" });
-};
+});
 
 // @desc Set projects
 // @route POST /api/projects
 // @access Private
-const setProject = (req, res) => {
+const setProject = asyncHandler(async (req, res) => {
+  if (!req.body.text) {
+    res.status(400);
+    throw new Error("Please add a text field");
+  }
   res.status(200).json({ message: "Set projects" });
-};
+});
 // @desc Update projects
 // @route PUT /api/projects/id
 // @access Private
-const updateProject = (req, res) => {
+const updateProject = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Update goal ${req.params.id}` });
-};
+});
 // @desc Delete projects
 // @route DELETE /api/projects/id
 // @access Private
-const deleteProject = (req, res) => {
+const deleteProject = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Delete goal ${req.params.id}` });
-};
+});
 
 module.exports = { getProjects, setProject, updateProject, deleteProject };
