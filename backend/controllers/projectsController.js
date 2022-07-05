d345f799b7de574331199b99f7a8aa5935a44367
heyjochen <jochen@stierberger.com>
2022-07-05 11:18:05 -0500

    include user auth to not delete projects of others

--- backend/controllers/projectsController.js
+++ backend/controllers/projectsController.js
@@ -1,11 +1,12 @@
 const asyncHandler = require("express-async-handler");
 const Project = require("../models/projectModel");
+const User = require("../models/userModel");
 
 // @desc Get projects
 // @route GET /api/projects
 // @access Private
 const getProjects = asyncHandler(async (req, res) => {
-  const projects = await Project.find();
+  const projects = await Project.find({ user: req.user.id });
   res.status(200).json(projects);
 });
 
@@ -25,6 +26,7 @@ const setProject = asyncHandler(async (req, res) => {
     film: req.body.film || "No Film provided",
     date: req.body.date || "No Date provided",
     notes: req.body.notes || "No Notes added",
+    user: req.user.id,
   });
   res.status(200).json(project);
 });
@@ -39,6 +41,21 @@ const updateProject = asyncHandler(async (req, res) => {
     res.status(400);
     throw new Error("Project not found");
   }
+
+  const user = await User.findById(req.user.id);
+
+  // Check for user
+  if (!user) {
+    res.status(401);
+    throw new Error("User not found");
+  }
+
+  // Make sure the logged in user matches the goal user
+  if (global.user.toString() !== user.id) {
+    res.status(401);
+    throw new Error("User not authorized");
+  }
+
   const updatedProject = await Project.findByIdAndUpdate(
     req.params.id,
     req.body,
@@ -56,6 +73,21 @@ const deleteProject = asyncHandler(async (req, res) => {
     res.status(400);
     throw new Error("Project not found");
   }
+
+  const user = await User.findById(req.user.id);
+
+  // Check for user
+  if (!user) {
+    res.status(401);
+    throw new Error("User not found");
+  }
+
+  // Make sure the logged in user matches the goal user
+  if (project.user.toString() !== user.id) {
+    res.status(401);
+    throw new Error("User not authorized");
+  }
+
   await project.remove();
   res.status(200).json({
     message: `Deleted project with title ${req.body.title} and ID ${req.params.id}`,
