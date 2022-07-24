bcf40565e3c53a08ac0e87a6ab5b18c0eaadafa0
heyjochen <jochen@stierberger.com>
2022-07-24 16:59:37 -0500

    include backend route for newsletter signup

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use('/api/projects', require('./routes/projectRoutes'));
 app.use('/api/users', require('./routes/userRoutes'));
+app.use('/api/newsletter', require('./routes/newsletter'));
 
 // Serve frontend
 if (process.env.NODE_ENV === 'production') {
